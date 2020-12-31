bdb8da55e7e5e4e75834331126adccb8668d1194
kazuma-takeda <kazuma.takeda@claves.co.jp>
2020-12-31 20:38:56 +0900

    任意の文字列を除外してパスワードを生成することができるようにした

--- passwordGenerator/README.md
+++ passwordGenerator/README.md
@@ -1,8 +1,7 @@
 # 注意事項など
-
 - 基本的にコーディング時点での最新のChromeでの挙動のみしか確認していません。IEなど古いブラウザでは動作しない可能性がありますので、polyfillやBabelなどを使用してください
 
-- このジェネレーターは、フロントエンド言語の練習の為のツールであり、パスワード強度を保証するものではありませんので、あらかじめご了承ください。（）
+- 今回ツールを作成している目的はJavaScriptコーディングの練習の為であり、実用的なツールを目指すものではありません。セキュリティ性が高いものを作るには、アルゴリズムやより高度な関数関数などを使用する必要があります。
 
 ## 関連する動画URLなど
 - polyfill, Babelについて
@@ -13,6 +12,9 @@ https://youtu.be/pp_0uM-dy68
 
 - HTMLのformについて（再生リスト）
 https://youtube.com/playlist?list=PLv7E5OqNAIPw_uK_fcbnfgl6z1A69tSlo
+
+- Sass全般の基礎について（再生リスト）
+https://youtube.com/playlist?list=PLv7E5OqNAIPyp7_GMar2gMmXJfHmOstvo
 
 - JavaScript全般の基礎について（再生リスト）
 https://youtube.com/playlist?list=PLv7E5OqNAIPwvXgGnXqb0_4xlTdEV8W2b
--- passwordGenerator/script.js
+++ passwordGenerator/script.js
@@ -1,3 +1,9 @@
+// 除外する文字列
+const baseExcludesTexts = ['o', 'O', '0', 'I', 'l', '1'];
+document.getElementById('excludeTexts').value = baseExcludesTexts.join(',');
+
+
+// パスワードを生成する為の配列を作る
 const passwordTexts = [];
 for (let i = 0; i < 26; i++) {
   passwordTexts.push(String.fromCharCode('a'.charCodeAt(0) + i))
@@ -9,7 +15,7 @@ for (let i = 0; i < 10; i++) {
   passwordTexts.push(i)
 }
 
-const inputForm = document.forms.inputs,
+const inputs = document.forms.inputs,
       passwordListElement = document.getElementById('passwordList'),
 
       passwordLength = 6,
@@ -20,10 +26,13 @@ document.getElementById('generateButton').addEventListener('click', e => {
 
   passwordListElement.innerHTML = '';
 
+  const excludeTexts = inputs.excludeTexts.value.split(','),
+        excludedpasswordTexts = passwordTexts.filter(text => !(excludeTexts.includes(String(text))));
+
   for (let i = 0; i < generatedNumberOfTimes; i++) {
     let passwords = '';
     for (let i = 0; i < passwordLength; i++) {
-      passwords += passwordTexts[Math.floor(Math.random() * passwordTexts.length)];
+      passwords += excludedpasswordTexts[Math.floor(Math.random() * excludedpasswordTexts.length)];
     }
     const li = document.createElement('li'),
           input = document.createElement('input');
