a898672c718062734d4d94d27dac11a8d5787cfb
kazuma-takeda <kazuma.takeda@claves.co.jp>
2020-12-31 21:30:51 +0900

    ユーザーが生成するパスワードに含める文字列を選択できるようにした

--- passwordGenerator/script.js
+++ passwordGenerator/script.js
@@ -2,42 +2,62 @@
 const baseExcludesTexts = ['o', 'O', '0', 'I', 'l', '1'];
 document.getElementById('excludeTexts').value = baseExcludesTexts.join(',');
 
-
-// パスワードを生成する為の配列を作る
-const passwordTexts = [];
-for (let i = 0; i < 26; i++) {
-  passwordTexts.push(String.fromCharCode('a'.charCodeAt(0) + i))
-}
-for (let i = 0; i < 26; i++) {
-  passwordTexts.push(String.fromCharCode('A'.charCodeAt(0) + i))
-}
-for (let i = 0; i < 10; i++) {
-  passwordTexts.push(i)
-}
-
 const inputs = document.forms.inputs,
       passwordListElement = document.getElementById('passwordList'),
 
       passwordLength = 6,
       generatedNumberOfTimes = 3;
 
+
 document.getElementById('generateButton').addEventListener('click', e => {
   e.preventDefault();
 
-  passwordListElement.innerHTML = '';
+  const isIncludeEnglishLowercase = inputs.includeEnglishLowercase.checked,
+        isIncludeEnglishUppercase = inputs.includeEnglishUppercase.checked,
+        isIncludeNumber = inputs.includeNumber.checked;
 
-  const excludeTexts = inputs.excludeTexts.value.split(','),
-        excludedpasswordTexts = passwordTexts.filter(text => !(excludeTexts.includes(String(text))));
+  if (isIncludeEnglishLowercase || isIncludeEnglishUppercase || isIncludeNumber) {
 
-  for (let i = 0; i < generatedNumberOfTimes; i++) {
-    let passwords = '';
-    for (let i = 0; i < passwordLength; i++) {
-      passwords += excludedpasswordTexts[Math.floor(Math.random() * excludedpasswordTexts.length)];
+    passwordListElement.innerHTML = '';
+
+    // パスワードを生成する為の配列を作る
+    const passwordTexts = [];
+
+    if (isIncludeEnglishLowercase) {
+      for (let i = 0; i < 26; i++) {
+        passwordTexts.push(String.fromCharCode('a'.charCodeAt(0) + i))
+      }
     }
-    const li = document.createElement('li'),
-          input = document.createElement('input');
-    input.value = passwords;
-    li.appendChild(input);
-    passwordListElement.appendChild(li);
+
+    if (isIncludeEnglishUppercase) {
+      for (let i = 0; i < 26; i++) {
+        passwordTexts.push(String.fromCharCode('A'.charCodeAt(0) + i))
+      }
+    }
+
+    if (isIncludeNumber) {
+      for (let i = 0; i < 10; i++) {
+        passwordTexts.push(i)
+      }
+    }
+
+
+    const excludeTexts = inputs.excludeTexts.value.split(','),
+          excludedpasswordTexts = passwordTexts.filter(text => !(excludeTexts.includes(String(text))));
+
+    for (let i = 0; i < generatedNumberOfTimes; i++) {
+      let passwords = '';
+      for (let i = 0; i < passwordLength; i++) {
+        passwords += excludedpasswordTexts[Math.floor(Math.random() * excludedpasswordTexts.length)];
+      }
+      const li = document.createElement('li'),
+            input = document.createElement('input');
+      input.value = passwords;
+      li.appendChild(input);
+      passwordListElement.appendChild(li);
+    }
+
+  } else {
+    alert(`「パスワードを構成する文字」には、少なくとも１つ以上の項目にチェックを入れてください）`)
   }
 })
